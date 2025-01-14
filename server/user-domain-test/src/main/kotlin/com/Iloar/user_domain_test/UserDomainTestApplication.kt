cf43dea5ac70358f59f3696ba0721ddd4ceca103
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2025-01-15 04:25:40 +0900

    first commit

---
+++ server/user-domain-test/src/main/kotlin/com/Iloar/user_domain_test/UserDomainTestApplication.kt
@@ -0,0 +1,11 @@
+package com.Iloar.user_domain_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserDomainTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserDomainTestApplication>(*args)
+}
---
+++ server/user-domain-test/src/test/kotlin/com/Iloar/user_domain_test/UserDomainTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Iloar.user_domain_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserDomainTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
